4c78a1c30651b7f0a3c6da5a24f29c2ef461fa76
TomoyaIshida <ishidatomoya@ishidatomoya-no-MacBook-Pro.local>
2017-06-11 00:08:19 +0900

    edit-message-view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,7 +12,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to root_path, notice: "グループを作成しました"
+      redirect_to group_messages_path(@group.id), notice: "グループを作成しました"
     else
       render :new
     end
@@ -23,7 +23,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to root_path, notice: "チャットグループが更新されました"
+      redirect_to group_messages_path(@group.id), notice: "チャットグループが更新されました"
     else
       render :edit
     end
