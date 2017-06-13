aa626a90db3c8b734d5e5abedba543c0b73fd48a
TomoyaIshida <ishidatomoya@ishidatomoya-no-MacBook-Pro.local>
2017-06-13 18:26:44 +0900

    test-message-model

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
 
   factory :message do
-    body         "Hello"
+    body         { Faker::Lorem.sentence}
     image        {File.open(File.join(Rails.root, '/spec/fixtures/files/test.jpg'))}
     group_id     "2"
     user_id      "1"
