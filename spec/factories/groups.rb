4c2c551c5eb4ddf6ed45131ff3bca61514cc760f
TomoyaIshida <ishidatomoya@ishidatomoya-no-MacBook-Pro.local>
2017-06-15 13:54:52 +0900

    test-message-controller

---
+++ spec/factories/groups.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+
+  factory :group do
+    groupname  Faker::Name.name
+  end
+end
--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -3,7 +3,10 @@ FactoryGirl.define do
   factory :message do
     body         { Faker::Lorem.sentence}
     image        {File.open(File.join(Rails.root, '/spec/fixtures/files/test.jpg'))}
-    group_id     "2"
-    user_id      "1"
+  end
+
+  trait :with_user_and_group do
+    user
+    group
   end
 end
