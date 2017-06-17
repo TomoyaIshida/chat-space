562062511faf582a78cb3e00677ec13ed81e2392
TomoyaIshida <ishidatomoya@ishidatomoya-no-MacBook-Pro.local>
2017-06-17 20:09:18 +0900

    incremental-search

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,11 @@
 $(function() {
+  function new_message(data) {
+    var new_message = '<div class="chat-group-user clearfix">' +
+                '<p class = "chat-group-user__name">' + data.name + '</p>' +
+                '<a class = "user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="data.id" data-user-name="data.name">追加</a>' +
+                '</div>';
+    return new_message;
+  }
   $(document).on('keyup', '#user-search-field', function(e){
     e.preventDefault();
     var input = $.trim($(this).val());
@@ -11,10 +18,14 @@ $(function() {
       dataType: 'json'
     })
     .done(function(data){
-      console.log(input);
+      var $form = $('#user-search-result')
+      $form.find('li').remove();
+      $(data).each (function(i, user){
+      $form.append('<li>' + new_message(user) + '</li>')
+      });
     })
     .fail(function(data) {
-      alert('error');
+      alert('ユーザー検索に失敗しました');
     });
   });
 });
