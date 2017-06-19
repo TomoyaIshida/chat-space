beb5e390a50c157b10f668edf1026ff0384b7657
TomoyaIshida <ishidatomoya@ishidatomoya-no-MacBook-Pro.local>
2017-06-19 15:45:38 +0900

    edit-incremental-search

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,19 +1,19 @@
 $(function() {
 
   function add_member(user) {
-    var add_member = '<li><div class="chat-group-user clearfix">' +
+    var add_member = '<li>' + '<div class="chat-group-user clearfix">' +
                 '<p class = "chat-group-user__name">' + user.name + '</p>' +
                 '<a class = "user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id= "' + user.id +'" data-user-name= "'+ user.name +'"+>追加</a>' +
-                '</div></li>';
+                '</div>' + '</li>';
     return add_member;
   }
 
  function new_member(user_id, user_name) {
-    var new_member = '<li><div class="chat-group-user clearfix js-chat-member" id="chat-group-user-8">' +
+    var new_member = '<li>' + '<div class="chat-group-user clearfix js-chat-member" id="chat-group-user-8">' +
                 '<input name="group[user_ids][]" type="hidden" value="' + user_id + '">' +
                 '<p class = "chat-group-user__name">' + user_name + '</p>' +
                 '<a class = "user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</a>' +
-                '</div></li>';
+                '</div>' + '</li>';
     return new_member;
   }
 
