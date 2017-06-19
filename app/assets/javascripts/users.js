549b04b9c7d20952849b27cf5fed19831c0677c3
TomoyaIshida <ishidatomoya@ishidatomoya-no-MacBook-Pro.local>
2017-06-19 15:41:00 +0900

    edit-incremental-search

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,20 +1,19 @@
 $(function() {
 
   function add_member(user) {
-    var add_member = '<div class="chat-group-user clearfix">' +
+    var add_member = '<li><div class="chat-group-user clearfix">' +
                 '<p class = "chat-group-user__name">' + user.name + '</p>' +
                 '<a class = "user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id= "' + user.id +'" data-user-name= "'+ user.name +'"+>追加</a>' +
-                '</div>';
+                '</div></li>';
     return add_member;
   }
 
  function new_member(user_id, user_name) {
-    console.log("new_member")
-    var new_member = '<div class="chat-group-user clearfix js-chat-member" id="chat-group-user-8">' +
+    var new_member = '<li><div class="chat-group-user clearfix js-chat-member" id="chat-group-user-8">' +
                 '<input name="group[user_ids][]" type="hidden" value="' + user_id + '">' +
                 '<p class = "chat-group-user__name">' + user_name + '</p>' +
                 '<a class = "user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</a>' +
-                '</div>';
+                '</div></li>';
     return new_member;
   }
 
@@ -35,7 +34,7 @@ $(function() {
       $form.empty();
 
       $(data).each (function(i, user){
-        $form.append('<li>' + add_member(user) + '</li>');
+        $form.append(add_member(user));
       });
     })
 
@@ -49,7 +48,7 @@ $(function() {
     var user_name = $(this).attr('data-user-name');
     var $member = $('#chat-group-users');
     $(this).parent().remove();
-    $member.append('<li>' + new_member(user_id, user_name) + '</li>');
+    $member.append(new_member(user_id, user_name));
   });
 
   $('.chat-group-form__field').on('click', '.user-search-remove', function(){
